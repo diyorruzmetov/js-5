7bf14a0026f735dcfe8a564eea746f32d6c91140
diyor <diyor@gmail.com>
2021-12-02 17:55:43 +0300

    js

---
+++ script.js
@@ -0,0 +1,9 @@
+let a = prompt('Введите своё имя');
+let b = prompt('Введите свой год рождения');
+let c = prompt('Введите нынешний год');
+
+function name(num, num2 ) {
+  return c - b
+}
+
+alert(a + ' , Ваш возраст ' + name());
